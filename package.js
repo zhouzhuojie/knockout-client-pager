255fb32a55e798a6fc3a13bd21558ac664de1894
zhouzhuojie <zhouzhuojie@gmail.com>
2013-08-29 00:41:48 -0400

    Change file structure, add more safe export main.js

--- package.js
+++ package.js
@@ -6,7 +6,8 @@ Package.on_use(function (api) {
   api.use('knockout-client', 'client');
   api.imply('knockout-client', 'client');
   api.use('jquery', 'client');
-  api.add_files(['lib/pager.js', 'lib/pager.css'], 'client');
+  api.add_files(['pagerjs/dist/pager.min.js', 'pagerjs/dist/pager.css'], 'client');
+  api.add_files('main.js', 'client');
   if (api.export) {
     api.export('pager', 'client');
   }
